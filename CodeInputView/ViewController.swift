7e11d77fb8ff2dd23eda87657a69feff26b544ac
lianxinbo <lianxinbo@hengli.com>
2021-03-22 10:04:22 +0800

    Initial Commit

---
+++ CodeInputView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CodeInputView
+//
+//  Created by lian on 2021/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
